9eb674aef99eb51469f5f3c9bf6b435fdb4317f1
hyKim <rlagjsdud3@hansung.ac.kr>
2026-08-16 17:16:04 +0900

    chore: MSW 계층을 걷어낸다 — 전 도메인 실서버 연동 완료

--- src/mocks/handlers/index.js
+++
@@ -1,5 +0,0 @@
-import { bagHandlers } from './bag.js'
-import { boardingPassHandlers } from './boardingPass.js'
-import { passportHandlers } from './passport.js'
-
-export const handlers = [...bagHandlers, ...boardingPassHandlers, ...passportHandlers]
